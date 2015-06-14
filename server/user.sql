416928ce1edd5f405585b56ab47af896afcbc61a
Armour <497052684@qq.com>
2015-06-14 19:35:36 +0800

    modified user.sql

--- server/user.sql
+++ server/user.sql
@@ -3,8 +3,25 @@ CREATE DATABASE IF NOT EXISTS eat;
 USE eat;
 
 CREATE TABLE `user` (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	username varchar(30) DEFAULT NULL,
-	password varchar(20) DEFAULT NULL,
-	PRIMARY KEY (id)
+    id int(11) NOT NULL AUTO_INCREMENT,
+    username varchar(30) NOT NULL,
+    password varchar(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `restaurant` (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    picture varchar(30) DEFAULT 'default.jpg',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `evaluate` (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    user_id int(11) NOT NULL,
+    restaurant_id int(11) NOT NULL,
+    score int(8) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
