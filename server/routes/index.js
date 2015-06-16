1906834ecdb65d088bae5d0cba5ec88e9796accd
Armour <497052684@qq.com>
2015-06-16 10:00:06 +0800

    add update_recommend.sh

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var sys = require('sys')
+var exec = require('child_process').exec;
 var mysql = require('mysql');
 var indexController = require('../controller/index.js');
-var recommend = require('../../db\ \&\ algorithm/recommend.json');
 
+var child;
+var recommend = require('../../db\ \&\ algorithm/recommend.json');
 var client = mysql.createConnection(require('../dbConfig.json'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var user = [];
     var restaurant = [];
+
+    child = exec("bash update_recommend.sh", function(error, stdout, stderr) {
+        console.log("recommend update!");
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
 
     client.query('SELECT * FROM user WHERE username=?', [req.query.username],
         function(err, rows) {
